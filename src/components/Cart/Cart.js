031950069c11f655524d3e39e4167e9cc34905e9
sabbir1020 <gazisabbir924@gamil.com>
2022-11-12 15:49:04 +0600

    toast added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = ({ carts }) => {
     setCart(time);
     addToCart(time);
   };
-  const notify = () => toast("Wow so easy!");
+  const notify = () => toast.success("Wow so easy!");
   const totalTime = carts.reduce(
     (previousValue, currentValue) => previousValue + currentValue.time,
     0
